71e1e6d0f65183b3bf033d87804fce54b4a9bc2e
sunyinge <sunyinge@eforms.com.cn>
2020-08-06 14:21:43 +0800

    '全局context'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import ThemeContext from "./theme-context";
+import ThemedBar from "./components/ThemedBar";
 import './App.css';
 import NameCard from './components/NameCard';
 import LikesButton from "./components/likesButton";
@@ -7,15 +9,30 @@ import DigitalClock from "./components/DigitalClock"
 import CommentBox from "./components/CommentBox";
 import CommentList from "./components/CommentList";
 
+const themes = {
+    light: {
+        classnames: 'btn btn-primary',
+        bgColor: '#eeeeee',
+        color: '#000'
+    },
+    dark: {
+        classnames: 'btn btn-light',
+        bgColor: '#222222',
+        color: '#fff'
+    }
+}
+
 const tags = ['恐龙', '仙女']
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            comments: ['this is my first reply']
+            comments: ['this is my first reply'],
+            theme: 'light'
         }
         this.addComment = this.addComment.bind(this)
         this.getIndex = this.getIndex.bind(this)
+        this.changeTheme = this.changeTheme.bind(this)
     }
     addComment(comment) {
         this.setState({
@@ -29,22 +46,38 @@ class App extends Component {
             comments: list
         })
     }
+    changeTheme(theme) {
+        this.setState({
+            theme,
+        })
+    }
     render() {
         const { comments } = this.state
         return (
+            <ThemeContext.Provider value={themes[this.state.theme]}>
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="app-logo" alt={logo}/>
                     <h1 className="App-title">Welcome to React</h1>
+                    <a href="#theme-switcher"
+                       className="btn btn-light"
+                       onClick={() => {this.changeTheme('light')}}
+                    >浅色主题</a>
+                    <a href="#theme-switcher"
+                       className="btn btn-secondary"
+                       onClick={() => {this.changeTheme('dark')}}
+                    >深色主题</a>
                 </header>
-                <CommentList comments={comments}
-                             onGetIndex={this.getIndex}
-                />
-                <CommentBox
-                    commentsLength={comments.length}
-                    onAddComment={this.addComment}
-                />
+                <ThemedBar />
+                {/*<CommentList comments={comments}*/}
+                {/*             onGetIndex={this.getIndex}*/}
+                {/*/>*/}
+                {/*<CommentBox*/}
+                {/*    commentsLength={comments.length}*/}
+                {/*    onAddComment={this.addComment}*/}
+                {/*/>*/}
             </div>
+            </ThemeContext.Provider>
         )
     }
 }
